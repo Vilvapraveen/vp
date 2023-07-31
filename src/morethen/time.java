d5a1b5264a2c3f533354662856f6dbc50062a457
vilvapraveen <vilvapraveen2@gmail.com>
2023-07-31 10:27:32 +0530

    Initial commit

---
+++ src/morethen/time.java
@@ -0,0 +1,15 @@
+package morethen;
+
+public class time {
+	public static void main(String[] args){
+		
+		int apple = 45,
+			banana = 30,
+			cake = 50,
+			dosa = 75,
+			total = apple+banana+cake+dosa;
+		
+		System.out.println("apple with banana "+(apple+banana)+("total "+total ));
+	}
+
+}
