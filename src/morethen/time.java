5123da8964b603391f1f4c0edd50cacf019149a9
vilvapraveen <vilvapraveen2@gmail.com>
2023-08-01 18:09:55 +0530

    Initial commit

--- src/morethen/time.java
+++ src/morethen/time.java
@@ -1,15 +1,25 @@
 package morethen;
 
 public class time {
+
 	public static void main(String[] args){
-		
-		int apple = 45,
+			int apple = 45,
 			banana = 30,
 			cake = 50,
-			dosa = 75,
-			total = apple+banana+cake+dosa;
+			dosa = 75;
+
+		int a,b,c,d,w,x,y,z;
+		System.out.println("product 	Quantity	Unit Price	Total"
+				+"\n-----------------------------------------------------"
+				+ "\napple"	+"		"+(a=(2))+"		"+apple+"		"+(w=(a*apple))
+				+ "\nbanana"+"		"+(b=(7))+"		"+banana+"	   	"+(x=(b*banana))
+				+ "\ncake"+"		"+(c=(3))+"		"+cake+"		"+(y=(c*cake))
+				+ "\ndosa"+"		"+(d=(2))+"		"+dosa+"		"+(z=(d*dosa))
+				+"\n				subtotal"+"	"+(w+x+y+z)
+				+"\n\nthank for coming back!");
 		
-		System.out.println("apple with banana "+(apple+banana)+("total "+total ));
+		
+		
 	}
 
 }
